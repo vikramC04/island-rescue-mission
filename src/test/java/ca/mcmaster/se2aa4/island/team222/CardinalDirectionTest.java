f17156af8f649a08d7c92788ad5ac109f2a6a106
vikramC04 <vikram.chandar04@gmail.com>
2024-03-21 11:21:11 -0400

    CardinalDirection Tests (close #59)

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team222/CardinalDirectionTest.java
@@ -0,0 +1,29 @@
+package ca.mcmaster.se2aa4.island.team222;
+
+import org.junit.jupiter.api.Test;
+
+import ca.mcmaster.se2aa4.island.team222.Directions.CardinalDirection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CardinalDirectionTest {
+    @Test
+    public void testNextRight() {
+        assertEquals(CardinalDirection.N, CardinalDirection.W.nextRight());
+        assertEquals(CardinalDirection.E, CardinalDirection.N.nextRight());
+        assertEquals(CardinalDirection.S, CardinalDirection.E.nextRight());
+        assertEquals(CardinalDirection.W, CardinalDirection.S.nextRight());
+        
+    }
+
+    @Test
+    public void testNextLeft() {
+        assertEquals(CardinalDirection.N, CardinalDirection.E.nextLeft());
+        assertEquals(CardinalDirection.E, CardinalDirection.S.nextLeft());
+        assertEquals(CardinalDirection.S, CardinalDirection.W.nextLeft());
+        assertEquals(CardinalDirection.W, CardinalDirection.N.nextLeft());
+    }
+
+
+}
